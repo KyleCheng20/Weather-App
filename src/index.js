4c73ada316e3352f31a4b537b3bfd8a8211ac3a3
Kyle Cheng <kcheng120@gmail.com>
2026-01-27 18:09:57 -0500

    Create processWeatherData function to process weather data json into custom object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import "./style.css";
 import { getWeatherData } from "./modules/getWeatherData";
+import { processWeatherData } from "./modules/processWeatherData";
 
 const API_KEY = "8V8B5T4ADFQXMCR6T35AH89ZZ";
 
-getWeatherData("philadelphia", API_KEY);
+const data = await getWeatherData("philadelphia", API_KEY);
+
+processWeatherData(data);
